874b829779d5960096f01b2a907eff0912293751
Nick <144842765+Nexexexexed@users.noreply.github.com>
2025-06-10 22:05:37 +0300

    adding card-component and 1 requset api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfileService } from './data/services/profile.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Social_Network_Tik_Talk';
+  profileServices: ProfileService = inject(ProfileService);
+  profiles: any = [];
+
+  constructor() {
+    this.profileServices.getTestAccount().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
 }
